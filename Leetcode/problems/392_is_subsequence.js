48684e5531326c26fa95c6c75612b48798a0cbf9
Sasa Trivic <sasatrivic92@gmail.com>
2022-11-28 23:51:37 +0100

    leetcode 392

---
+++ Leetcode/problems/392_is_subsequence.js
@@ -0,0 +1,19 @@
+// Time: O(n) - where n is the length of string t
+// Space: O(1) - we are only storing 3
+
+var isSubsequence = function(s, t) {
+    let i = 0
+    let j = 0
+    let hits = 0
+    while (i < s.length && j < t.length) {
+        if (s[i] == t[j]) {
+            i++
+            hits++
+            if (hits == s.length) {
+                return true
+            }
+        }
+        j++
+    }
+    return hits == s.length
+};
