cfff539cf377a13d6c7b69a271707245bbca1a9b
Sasa Trivic <sasatrivic92@gmail.com>
2023-01-06 14:37:20 +0100

    Leetcode 589 iterative solution

--- Leetcode/problems/589_nary_tree_preorder.js
+++ Leetcode/problems/589_nary_tree_preorder.js
@@ -15,3 +15,22 @@ var preorder = function(root) {
 
 // Time: O(n)
 // Space: O(n)
+
+// Iterative approach
+var preorder = function(root) {
+    if (!root) {
+        return [];
+    }
+    let stack = [root];
+    let vals = [];
+    while (stack.length) {
+        let current = stack.pop();
+        vals.push(current.val);
+        stack = stack.concat(current.children.reverse());
+    }
+    return vals;
+};
+
+// Same complexity
+// Time: O(n) - one loop for each node when we take it out of stack
+// Space: O(n) - we are just moving elements from stack to vals so combined they have at most n elements
