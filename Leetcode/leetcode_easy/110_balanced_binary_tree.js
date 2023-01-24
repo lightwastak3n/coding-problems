2d720d5ed9ca9d2865f4af308fa1003438df5ad2
Sasa Trivic <sasatrivic92@gmail.com>
2023-01-25 00:19:31 +0100

    Edit Leetcode 110. Add to tables.

--- Leetcode/leetcode_easy/110_balanced_binary_tree.js
+++ Leetcode/leetcode_easy/110_balanced_binary_tree.js
@@ -12,3 +12,8 @@ var isBalanced = function(root) {
     }
     return getHeight(root) != -1; 
 };
+
+// getHeight recursively traverses tree and compares left and right subtrees
+// If there is difference of more than 1 between them it returns -1 otherwise it returns 0 or height of the tree.
+// Time complexity: O(n) - checking every node only once
+// Space complexity: O(h) - h is height of the tree, for balanced tree log(n) and for a skewed n
